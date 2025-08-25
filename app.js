752fa21fbad9bedf74ec00789212218208afb8c5
BRENO OLIVEIRA ASSIS REIS <25133656@cfp127aluno.local>
2025-08-25 10:24:52 -0300

    Gerenciador de tarelas com array

---
+++ app.js
@@ -0,0 +1,22 @@
+/* TypeScript
+ * ARRAY
+ * - Uma array é uma estrutura de dados projetados para armazenar coleções de dados.
+ * - Elementos: um array é uma coleção de valores, chamados elementos. 
+ * - Ordenação: os elementos são organizados em uma ordem definida, acessados por um índice.
+ * - idexação iniciada em zero.
+ * - Tamanho dinâmico: pode crescer ou diminuir conforme elementos são adicionados ou removidos.
+ * - Heterogeneidade: é possivel armazenar diferentes tipos de dados no mesmo array (número, string, objeto e até outros arrays)
+ *  Select para retornar uma "lista" de informações, que é convertido em um array que também lista.
+ * 
+ * Métodos de manipular de array
+ * Métodos:
+ *  - push():adiciona no final
+ *  - pop():remove do final
+ *  - unshift():adiciona no início
+ *  - shift():remove do início
+ *  - splice():permite ter o controle sobre o elemento que deve ser removido ou adicionar
+ * 
+ * Método push
+ *  - adiciona um ou mais elementos ao final do array
+ *  - retorna o novo tamanho do array
+ */
